996f3b0f6fddef0774c01739283b4d63c83ba7b0
ychen <hzchenyuefang@corp.netease.com>
2015-03-10 16:38:55 +0800

    add localrc, vimrc and openrc

---
+++ admin-openrc.sh
@@ -0,0 +1,11 @@
+export OS_USERNAME=admin
+export OS_PASSWORD=ntse 
+export OS_TENANT_NAME=admin 
+export OS_AUTH_URL=http://localhost:5000/v2.0/ 
+export OS_AUTH_STRATEGY=keystone
+#the following two variables were only used to bootstrap the adminisrative user and register the Identity Service
+#export OS_SERVICE_TOKEN=ntse  
+#export OS_SERVICE_ENDPOINT=http://localhost:35357/v2.0
+#unset  OS_SERVICE_TOKEN    
+#unset  OS_SERVICE_ENDPOINT
+
